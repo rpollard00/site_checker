1eca3a344ca9994121ab17403ac66bb17461d68c
Reese Pollard <rpollard@gmail.com>
2024-07-06 22:12:05 -0700

    Load configuration and site list from json

--- src/main.zig
+++ src/main.zig
@@ -1,80 +1,151 @@
 const std = @import("std");
 const print = std.debug.print;
+const json = std.json;
 const Timer = std.time.Timer;
+const TcpConnectToHostError = std.net.TcpConnectToHostError;
+
+const Site = struct {
+    name: []const u8,
+    port: u16 = 443, // use 443 as a default
+};
+
+const Config = struct {
+    sites: []Site,
+    pollingIntervalMs: u64,
+};
+
+pub fn loadConfigFile(alloc: std.mem.Allocator, config_file: []const u8) !std.json.Parsed(Config) {
+    const max_config_size = 2000;
+
+    const file = try std.fs.cwd().openFile(config_file, .{});
+    defer file.close();
+
+    const contents = try file.reader().readAllAlloc(alloc, max_config_size);
+    defer alloc.free(contents);
+
+    return try std.json.parseFromSlice(Config, alloc, contents, .{ .allocate = .alloc_always });
+}
 
 const SiteChecker = struct {
     allocator: std.mem.Allocator,
-    siteList: std.ArrayList([]const u8),
+    sites: std.ArrayList(Site),
     timer: Timer,
 
     pub fn init(alloc: std.mem.Allocator) !SiteChecker {
         return SiteChecker{
             .allocator = alloc,
-            .siteList = std.ArrayList([]const u8).init(alloc),
+            .sites = std.ArrayList(Site).init(alloc),
             .timer = try Timer.start(),
         };
     }
 
     pub fn deinit(self: *SiteChecker) void {
-        self.siteList.deinit();
+        self.sites.deinit();
     }
 
-    pub fn startCheckSiteList(self: *SiteChecker) !void {
-        for (self.siteList.items) |site| {
-            _ = self.checkSite(site) catch |err| switch (err) {
-                error.UnknownHostName => {
-                    print("Unknown hostname {s}", .{site});
+    pub fn pollSites(self: *SiteChecker) !void {
+        for (self.sites.items) |site| {
+            _ = self.checkSite(site.name, site.port) catch |err| {
+                if (recoverableNetworkErrorClassifier(err)) |recoverable_error| {
+                    try handleRecoverableNetworkError(site.name, recoverable_error);
+                } else {
                     return err;
-                },
-                else => return err,
+                }
             };
         }
     }
 
-    fn checkSite(self: *SiteChecker, site_url: []const u8) !u64 {
+    fn handleRecoverableNetworkError(site: []const u8, err: RecoverableNetworkError) !void {
+        switch (err) {
+            RecoverableNetworkError.UnknownHostName => {
+                print("Unknown hostname {s}\n", .{site});
+            },
+            RecoverableNetworkError.ConnectionRefused => {
+                print("Connection refused by {s}\n", .{site});
+            },
+            RecoverableNetworkError.ConnectionTimedOut => {
+                print("Connection to {s} timed out\n", .{site});
+            },
+            RecoverableNetworkError.ConnectionResetByPeer => {
+                print("Connection to {s} reset by peer\n", .{site});
+            },
+            RecoverableNetworkError.NameServerFailure, RecoverableNetworkError.TemporaryNameServerFailure => {
+                print("Name Server Failure when resolving {s}\n", .{site});
+            },
+            RecoverableNetworkError.NetworkUnreachable => {
+                print("Received Network Unreachable error when connecting to {s}\n", .{site});
+            },
+        }
+    }
+
+    fn checkSite(self: *SiteChecker, site_url: []const u8, site_port: u16) !u64 {
         const start: u64 = self.timer.read();
-        print("Connecting to site: {s}...", .{site_url});
-        var stream = try std.net.tcpConnectToHost(self.allocator, site_url, 443);
+        print("Polling {s}...", .{site_url});
+        errdefer print("\n", .{});
+
+        var stream = try std.net.tcpConnectToHost(self.allocator, site_url, site_port);
+
         defer stream.close();
 
         const now: u64 = self.timer.lap();
         const duration_in_ms: u64 = (now - start) / NS_IN_MS;
 
-        print("RTT: {d} ms\n", .{duration_in_ms});
+        print("Success [RTT {d}ms]\n", .{duration_in_ms});
 
         return duration_in_ms;
     }
 };
 
+const RecoverableNetworkError = error{
+    ConnectionRefused,
+    NetworkUnreachable,
+    ConnectionTimedOut,
+    ConnectionResetByPeer,
+
+    UnknownHostName,
+    // check that these are what I think they are
+    TemporaryNameServerFailure,
+    NameServerFailure,
+};
+
+pub fn recoverableNetworkErrorClassifier(err: anyerror) ?RecoverableNetworkError {
+    return switch (err) {
+        error.ConnectionRefused => RecoverableNetworkError.ConnectionRefused,
+        error.NetworkUnreachable => RecoverableNetworkError.NetworkUnreachable,
+        error.ConnectionTimedOut => RecoverableNetworkError.ConnectionTimedOut,
+        error.ConnectionResetByPeer => RecoverableNetworkError.ConnectionResetByPeer,
+        error.UnknownHostName => RecoverableNetworkError.UnknownHostName,
+        error.TemporaryNameServerFailure => RecoverableNetworkError.TemporaryNameServerFailure,
+        error.NameServerFailure => RecoverableNetworkError.NameServerFailure,
+        else => null,
+    };
+}
+
 // opens and closes a tcp socket to the given site_url
 // returns the response time in ms
 
 const NS_IN_MS = 1000000;
-const WAIT_TIME_MS = 5000;
 
 pub fn main() !void {
     var gpa = std.heap.GeneralPurposeAllocator(.{}){};
     defer _ = gpa.deinit();
     const allocator = gpa.allocator();
 
+    const parsedConfig = try loadConfigFile(allocator, "config.json");
+    defer parsedConfig.deinit();
+
+    const config = parsedConfig.value;
+
     var siteChecker = try SiteChecker.init(allocator);
     defer siteChecker.deinit();
 
-    try siteChecker.siteList.append("tellmewhatyouwant.lol");
-    try siteChecker.siteList.append("reesep.com");
+    for (config.sites) |site| try siteChecker.sites.append(site);
 
+    print("Starting site checker with an interval of {d}ms\n", .{config.pollingIntervalMs});
     while (true) {
-        print("Checking sites....\n", .{});
-
-        try siteChecker.startCheckSiteList();
-        print("Waiting {d}ms for next check...\n", .{WAIT_TIME_MS});
-        std.time.sleep(WAIT_TIME_MS * NS_IN_MS);
+        try siteChecker.pollSites();
+        std.time.sleep(config.pollingIntervalMs * NS_IN_MS);
     }
 }
 
-test "simple test" {
-    var list = std.ArrayList(i32).init(std.testing.allocator);
-    defer list.deinit(); // try commenting this out and see if zig detects the memory leak!
-    try list.append(42);
-    try std.testing.expectEqual(@as(i32, 42), list.pop());
-}
+const expect = std.testing.expect;
